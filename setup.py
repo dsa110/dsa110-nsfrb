5804642cccc8328b012b912d8180e7dade3a1ba8
nkosogor <nakosogorov@gmail.com>
2024-05-16 15:42:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
-from version import get_git_version
+#from version import get_git_version
 
 setup(name='dsa110-nsfrb',
       #version=get_git_version(),
       version="0.1.0",
       url='http://github.com/dsa110/dsa110-nsfrb',
       python_requires='>3.8',
-#      requirements=['seaborn', 'astropy', 'hdbscan', 'progress'],
-#      packages=['nsfrb'],
+      packages=['nsfrb'],
       zip_safe=False)
 
 #get local nsfrb directory
