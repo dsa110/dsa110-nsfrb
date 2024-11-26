e761b3a42a0dcdd85ac2d1914833cb02d96e2baf
mbsherma <mbsherm2017@gmail.com>
2024-11-26 18:56:24 +0000

    created systemctl services for clearing memory after 1 week

---
+++ offline/run_clearvis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ~/.bashrc
+source /home/ubuntu/msherman_nsfrb/miniconda/bin/activate casa310nsfrb
+/home/ubuntu/msherman_nsfrb/miniconda/envs/casa310nsfrb/bin/python /home/ubuntu/msherman_nsfrb/DSA110-NSFRB-PROJECT/dsa110-nsfrb/offline/clearvis.py --waittime 7 --cadence 2
