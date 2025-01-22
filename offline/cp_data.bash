ef309e8e5dec847431dbc8211c00f03ce56e8654
mbsherma <mbsherm2017@gmail.com>
2025-01-22 22:29:04 +0000

    vikram fixed GPUs on hh04, hh12, updated copy script to match

--- offline/cp_data.bash
+++ offline/cp_data.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-corrs=("h03" "h04" "h05" "h06" "hh07" "h08" "h10" "h11" "h12" "h14" "h15" "h16" "h18" "h19" "h21" "h22")
+corrs=("h03" "hh04" "h05" "h06" "hh07" "h08" "h10" "h11" "hh12" "h14" "h15" "h16" "h18" "h19" "h21" "h22")
 fullnames=("lxd110h03" "lxd110h04" "lxd110h05" "lxd110h06" "lxd110h07" "lxd110h08" "lxd110h10" "lxd110h11" "lxd110h12" "lxd110h14" "lxd110h15" "lxd110h16" "lxd110h18" "lxd110h19" "lxd110h21" "lxd110h22")
 sbs=("sb00" "sb01" "sb02" "sb03" "sb04" "sb05" "sb06" "sb07" "sb08" "sb09" "sb10" "sb11" "sb12" "sb13" "sb14" "sb15")
 
